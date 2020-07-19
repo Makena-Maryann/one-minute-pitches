96c9cfdc0d9e4a6cd131b5193fc958f26bfb4591
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-19 13:40:25 +0300

    Create destination for photos

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryann:Maryann00*@localhost/pitcher'
-    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     '''
