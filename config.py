b55209b893b45fd48c9448e175b30b4c99ffcf24
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-21 09:55:11 +0300

    Add test configurations

--- config.py
+++ config.py
@@ -5,8 +5,14 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryann:Maryann00*@localhost/pitcher'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
     '''
@@ -17,6 +23,15 @@ class ProdConfig(Config):
     '''
     pass
 
+class TestConfig(Config):
+    '''
+    Test configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryann:Maryann00*@localhost/pitcher_test'
+
 class DevConfig(Config):
     '''
     Development  configuration child class
@@ -24,9 +39,11 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryann:Maryann00*@localhost/pitcher'
     DEBUG = True
 
 config_options = {
  'development':DevConfig,
- 'production':ProdConfig
+ 'production':ProdConfig,
+ 'test':TestConfig
 }           
