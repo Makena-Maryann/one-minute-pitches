9a14284c8a1e1562e97f25de0c7e51f768cc6a57
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-21 09:16:55 +0300

    Update manage file to run tests

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
-from app.models import User,Pitch
+from app.models import User,Pitch,Comment
 
 app = create_app('development')
 
@@ -21,7 +21,6 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Pitch = Pitch)    
-
+    return dict(app = app,db = db,User = User,Pitch = Pitch,Comment = Comment)
 if __name__ == '__main__':
     manager.run()
