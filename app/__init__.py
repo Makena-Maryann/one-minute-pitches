e12b99d8b291b2b701a7fb7c94b6d5a20ab6ec0f
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-21 09:56:39 +0300

    Add flask-mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,16 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-photos = UploadSet('photos', IMAGES)
-
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -22,8 +23,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    configure_uploads(app, photos)
-
+    configure_uploads(app,photos)
+    mail.init_app(app)
+     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
